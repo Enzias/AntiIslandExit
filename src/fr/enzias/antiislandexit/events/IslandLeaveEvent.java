7140c6c87667a8fbe160f527d449758d5feea5bf
Enzias <68203437+Enzias@users.noreply.github.com>
2020-07-16 21:01:35 +0200

    Optimisation

--- src/fr/enzias/antiislandexit/events/IslandLeaveEvent.java
+++ src/fr/enzias/antiislandexit/events/IslandLeaveEvent.java
@@ -29,20 +29,25 @@ public class IslandLeaveEvent implements Listener {
 
             Location exit = event.getLocation();
             Location spawn = event.getIslandLocation();
+            Location playerLoc = player.getLocation();
 
             IntRange rangePositiveX = new IntRange(spawn.getX() + (event.getProtectionSize() / 2) + 1, spawn.getX() + (event.getProtectionSize() / 2) - 1);
             IntRange rangeNegativeX = new IntRange(spawn.getX() - (event.getProtectionSize() / 2) + 1, spawn.getX() - (event.getProtectionSize() / 2) - 1);
             IntRange rangePositiveZ = new IntRange(spawn.getZ() + (event.getProtectionSize() / 2) + 1, spawn.getZ() + (event.getProtectionSize() / 2) - 1);
             IntRange rangeNegativeZ = new IntRange(spawn.getZ() - (event.getProtectionSize() / 2) + 1, spawn.getZ() - (event.getProtectionSize() / 2) - 1);
-            
+
                 if (rangePositiveX.containsInteger(exit.getX())) {
-                    player.teleport(new Location(player.getWorld(), exit.getX() - 1, exit.getY(), exit.getZ(), player.getLocation().getYaw(), player.getLocation().getPitch()));
+                    playerLoc.setX(player.getLocation().getX()-0.35);
+                    player.teleport(playerLoc);
                 } else if (rangeNegativeX.containsInteger(exit.getX())) {
-                    player.teleport(new Location(player.getWorld(), exit.getX() + 1, exit.getY(), exit.getZ(), player.getLocation().getYaw(), player.getLocation().getPitch()));
+                    playerLoc.setX(player.getLocation().getX()+0.35);
+                    player.teleport(playerLoc);
                 } else if (rangePositiveZ.containsInteger(exit.getZ())) {
-                    player.teleport(new Location(player.getWorld(), exit.getX(), exit.getY(), exit.getZ() - 1, player.getLocation().getYaw(), player.getLocation().getPitch()));
+                    playerLoc.setZ(player.getLocation().getZ()-0.35);
+                    player.teleport(playerLoc);
                 } else if (rangeNegativeZ.containsInteger(exit.getZ())) {
-                    player.teleport(new Location(player.getWorld(), exit.getX(), exit.getY(), exit.getZ() + 1, player.getLocation().getYaw(), player.getLocation().getPitch()));
+                    playerLoc.setZ(player.getLocation().getZ()+0.35);
+                    player.teleport(playerLoc);
                 }
                 else if(plugin.getConfig().getBoolean("antiislandexit.lastchance")){
                     player.teleport(event.getIslandLocation());
